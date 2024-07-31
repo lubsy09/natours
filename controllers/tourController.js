f90d23a8e104b8bed592fcc8c63ccb5970ba050a
Lubabatu-Ahmad <lubabatuahmad60@gmail.com>
2024-07-31 12:32:26 +0100

    Chaining Multiple Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,7 @@ const tours = JSON.parse(
 
 exports.checckID = (req, res, next, val) => {
     console.log(`Tour id is: ${val}`);
-    
+
     if (req.params.id * 1 > tours.length) {
         return res.status(404).json({
             status: 'fail',
@@ -16,6 +16,15 @@ exports.checckID = (req, res, next, val) => {
     next();
 };
 
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        })
+    }
+    next();
+}
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
 

67639d7bf219fa2e9715b15dd96a7af60c513721
Lubabatu-Ahmad <lubabatuahmad60@gmail.com>
2024-08-06 13:33:15 +0100

    Using MongoDB with Mongoose and Creating Simple Tour Model

---
+++ playground-2.mongodb.js
@@ -0,0 +1,20 @@
+/* global use, db */
+
+
+// Select the database to use.
+use('mongodb+srv://lubsy:<password>@cluster0.zkvr39a.mongodb.net/natours');
+
+
+// Insert a few documents into the sales collection.
+
+
+// Run a find command to view items sold on April 4th, 2014.
+
+
+// Print a message to the output window.
+
+
+// Here we run an aggregation and open a cursor to the results.
+// Use '.toArray()' to exhaust the cursor to return the whole result set.
+// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+

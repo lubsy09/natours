7b19ff0b3aba67f6f9dc517fead374732a69fa71
Lubabatu-Ahmad <lubabatuahmad60@gmail.com>
2024-08-06 14:04:13 +0100

    Intro to BE Archy MVC Refractoring for MVC

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,7 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checckID);
+//  router.param('id', tourController.checckID);
 
 router
     .route('/')

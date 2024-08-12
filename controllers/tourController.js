85cd5e4f7ca5b69d43f0423f3537f6bbf0113cb8
Lubabatu-Ahmad <lubabatuahmad60@gmail.com>
2024-08-12 13:54:09 +0100

    Making the API better sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,28 @@
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
+
     //  BUILD QUERY
-    //  1) Filtering
+    //  1a) Filtering
    try { 
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
-    //  2) Advanced filtering 
+
+    //  1b) Advanced filtering 
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr)); 
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); 
     
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+
+    // 2) Sorting
+    if(req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
 
     //     EXECUTE QUERY
         const tours = await query;
